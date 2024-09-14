23b96f26d3f5ad9a6c84009500d67b6c57ce23f9
Lukas Maschmann <85629356+lukasmsn@users.noreply.github.com>
2024-09-14 22:42:39 +0200

    remove titles from feed

--- src/components/BitesTitle.tsx
+++ src/components/BitesTitle.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@/utils/supabase/server';
 
-export default async function BitesNoTitle() {
+export default async function Bites() {
   const supabase = createClient();
   const { data: bites } = await supabase.from("bites").select();
 
@@ -9,16 +9,17 @@ export default async function BitesNoTitle() {
     {bites?.map((bite, index) => (
       <div key={index} className="bite-entry hover:bg-gray-100 rounded-none sm:rounded-2xl pt-2 pb-3 md:pt-3 md:pb-4 px-5 cursor-pointer">
         <div className="space-y-0">
+          <h2 className="md:text-lg font-semibold">{bite.metadata.title}</h2>
           <p className="flex-col space-x-1">
-            <span className="author text-sm md:text-base text-gray-700 font-bold">
-              {bite.metadata.parentAuthor}
+            <span className="author text-sm md:text-base text-gray-400 font-medium">
+              {bite.metadata.parentAuthor} •
             </span>
             <span className="title text-sm md:text-base text-gray-400 font-medium">
-            • {bite.metadata.parentTitle}
+              {bite.metadata.parentTitle}
             </span>
           </p>
         </div>
-        <p className="content text-sm md:text-base">{bite.content}</p>
+        <p className="content mt-1 text-sm md:text-base">{bite.content}</p>
         {/* <div className="flex justify-between mt-2">
           <div className="flex justify-start space-x-2">
             <div className="bg-gray-100 h-9 rounded-full px-3 flex items-center cursor-pointer">
